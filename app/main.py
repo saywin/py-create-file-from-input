33113340b9ce20054abcb24b789b9bf24f5dc955
tiway <stilon670@gmail.com>
2024-03-22 12:34:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def main() -> None:
-    with open(f"{input("Enter name of the file: ")}.txt", "w") as file:
+    with open(f"{input('Enter name of the file: ')}.txt", "w") as file:
         user_text = input("Enter new line of content: ")
 
         while user_text != "stop":
